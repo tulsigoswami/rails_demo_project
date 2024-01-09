79814f18c3abc43adc03f443aa57664409555f6b
tulsigoswami <tulsig@shriffle.com>
2024-01-09 11:16:07 +0530

    wrote rspec for follow model & deleted files related to category

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,18 +2,16 @@ require 'rails_helper'
 
 class RecipeTest < ActiveSupport::TestCase
   RSpec.describe Recipe, type: :model do
-    describe "associations" do
+    describe "Associations" do
       it{ should belong_to(:user)}
-
       it{should have_many(:likes)}
-
       it{should have_many(:comments)}
-
       it{should have_one_attached(:content)}
     end
 
     describe "validations" do
       it { should validate_presence_of(:title) }
+      it { should validate_presence_of(:description) }
     end
   end
 end
